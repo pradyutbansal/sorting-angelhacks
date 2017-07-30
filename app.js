9431c7ee853897d0edf1e94abf5d2c83bd5c1249
Oberon <ahijaouy@gatech.edu>
2017-07-30 03:14:01 -0700

    extend bodyparser shit

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ var app = express();
 
 
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 
 app.get('/connect', function(req,res){
