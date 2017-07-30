e98e054a75751871f1b81d969aab68d7f4667cdb
Oberon <ahijaouy@gatech.edu>
2017-07-30 02:24:43 -0700

    lol hopefully this works

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+   "googleAuth" : {
+       "clientID"      : `925901605067-70hvre38sr0r2qh8ocm769kqbtj4bt41.apps.googleusercontent.com`,
+       "clientSecret"  : `m7FI4CXxnIVczSmJK2z-0_Mq`,
+       "callbackURL"   : `http://localhost:3000/connect/callback`
+   }
+};
